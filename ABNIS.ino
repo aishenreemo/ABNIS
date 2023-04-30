784c755d2a0e11e0f5be518cef6739176a94994e
Aishen <aish3n@pm.me>
2023-04-30 16:42:09 +0800

    feat: updates
    - remove sensitivity button
    - red and yellow indicators
    - decibel meter

--- ABNIS.ino
+++ ABNIS.ino
@@ -1,22 +1,14 @@
 #define ON_PIN 2
+#define YELLOW_PIN 3
+#define RED_PIN 4
 #define SOUND_PIN A0
-#define TRIGGER_PIN 13
-
-#define SENS_BTN_CD 500
-#define SENS_BTN_PIN 3
 
 #define ON_BTN_CD 500
-#define ON_BTN_PIN 4
-
-#define LEVEL_COUNT 3
+#define ON_BTN_PIN 8
 
 #define BLINK_DELAY 50
-
-const int levels[LEVEL_COUNT] = {
-    700,
-    540,
-    530,
-};
+#define YELLOW_THRESHOLD 55
+#define RED_THRESHOLD 60
 
 struct button_t {
     unsigned long last;
@@ -28,30 +20,23 @@ struct button_t sensitivity;
 struct button_t on;
 
 int turned_on;
-int level_cur;
-int level_ptr;
 int sound_level;
+
+int sound_read();
 
 void setup() {
     Serial.begin(9600);
     turned_on = LOW;
 
-    level_ptr = 0;
-    level_cur = levels[level_ptr];
-
-    sensitivity.last = SENS_BTN_CD;
-    sensitivity.cooldown = SENS_BTN_CD;
-    sensitivity.pin = SENS_BTN_PIN;
-
     on.last = ON_BTN_CD;
     on.cooldown = ON_BTN_CD;
     on.pin = ON_BTN_PIN;
 
-    pinMode(sensitivity.pin, INPUT_PULLUP);
     pinMode(on.pin, INPUT_PULLUP);
 
     pinMode(SOUND_PIN, INPUT);
-    pinMode(TRIGGER_PIN, OUTPUT);
+    pinMode(RED_PIN, OUTPUT);
+    pinMode(YELLOW_PIN, OUTPUT);
     pinMode(ON_PIN, OUTPUT);
 }
 
@@ -63,29 +48,46 @@ void loop() {
         on.last = millis();
     }
 
-    // listen to SENSITIVITY button
-    if (turned_on && digitalRead(sensitivity.pin) == LOW && millis() - sensitivity.last >= sensitivity.cooldown) {
-        level_ptr += 1;
-        level_ptr %= LEVEL_COUNT;
-        level_cur = levels[level_ptr];
-
-        for (int i = 0; i < level_ptr + 1; i++) {
-            digitalWrite(TRIGGER_PIN, HIGH);
-            delay(BLINK_DELAY);
-            digitalWrite(TRIGGER_PIN, LOW);
-            delay(BLINK_DELAY);
-        }
-
-        sensitivity.last = millis();
-    }
-
     if (!turned_on) return;
 
-    sound_level = analogRead(SOUND_PIN);
+    sound_level = sound_read();
 
-    if (sound_level >= level_cur) {
-        digitalWrite(TRIGGER_PIN, HIGH);
+    Serial.print("Sound level: ");
+    Serial.print(sound_level);
+    Serial.println(" dB");
+
+    if (sound_level >= RED_THRESHOLD) {
+        digitalWrite(RED_PIN, HIGH);
         delay(BLINK_DELAY * 5);
-        digitalWrite(TRIGGER_PIN, LOW);
+        digitalWrite(RED_PIN, LOW);
+    } else if (sound_level >= YELLOW_THRESHOLD) {
+        digitalWrite(YELLOW_PIN, HIGH);
+        delay(BLINK_DELAY * 5);
+        digitalWrite(YELLOW_PIN, LOW);
     }
+}
+
+int sound_read() {
+    float range = 0;
+    int sample_window = 50;
+
+    unsigned int sample;
+    unsigned int signal_max = 0;
+    unsigned int signal_min = 1024;
+
+    unsigned long start = millis();
+    while (millis() - start < sample_window) {
+        sample = analogRead(SOUND_PIN);
+
+        if (sample >= 1024) continue;
+
+        if (sample > signal_max) {
+            signal_max = sample;
+        } else if (sample < signal_min) {
+            signal_min = sample;
+        }
+    }
+
+    range = signal_max - signal_min;
+    return map(range, 20, 200, 49.5, 90);
 }
